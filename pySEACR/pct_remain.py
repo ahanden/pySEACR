946a9d41c5775f36627ea035055093dae3f95d14
ahanden <adam.handen@gmail.com>
2024-03-28 16:50:20 -0500

    linted pct_remain

--- pySEACR/pct_remain.py
+++ pySEACR/pct_remain.py
@@ -1,45 +1,43 @@
-"""
-PCT Remain functions from SEACR.
-"""
+"""PCT Remain functions from SEACR."""
 import warnings
 
 import numpy as np
 from scipy.stats import ecdf
 
 
-def pct_remain_vec(vec_1, vec_2, x):
+def pct_remain_vec(exp, ctrl, value):
     """
     Find the ratio of vectors' distributions.
 
     Parameters:
-        vec_1 (collection): Numerator
-        vec_2 (collection): Denominator
-        x (float): Value to test on
+        exp (collection): Numerator
+        ctrl (collection): Denominator
+        value (float): Value to test on
 
     Returns:
         float
     """
-    both = np.concatenate((vec_1, vec_2))
-    ecdf_1 = ecdf(vec_1).cdf
+    both = np.concatenate((exp, ctrl))
+    ecdf_exp = ecdf(exp).cdf
     ecdf_both = ecdf(both).cdf
-    top = len(vec_1) - ecdf_1.evaluate(x) * len(vec_1)
-    bottom = len(both) - ecdf_both.evaluate(x) * len(both)
-    with warnings.catch_warnings(action="ignore"):
+    top = len(exp) - ecdf_exp.evaluate(value) * len(exp)
+    bottom = len(both) - ecdf_both.evaluate(value) * len(both)
+    with warnings.catch_warnings(action='ignore'):
         return top / bottom
 
 
-def pct_remain_max(vec_1, vec_2, x):
+def pct_remain_max(exp, ctrl, value):
     """
     Find the ratio of two maxs' distributions.
 
     Parameters:
-        vec_1 (collection): First data set
-        vec_2 (collection): Second data set
-        x (float): Value to test
+        exp (collection): First data set
+        ctrl (collection): Second data set
+        value (float): Value to test
 
     Returns:
         float
     """
-    ecdf_1 = ecdf(vec_1).cdf
-    ecdf_2 = ecdf(vec_2).cdf
-    return 1 - (ecdf_1.evaluate(x) - ecdf_2.evaluate(x))
+    ecdf_exp = ecdf(exp).cdf
+    ecdf_ctrl = ecdf(ctrl).cdf
+    return 1 - (ecdf_exp.evaluate(value) - ecdf_ctrl.evaluate(value))
